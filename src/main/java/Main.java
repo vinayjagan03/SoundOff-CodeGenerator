870a229812585d8d58ea37957d6c906a0e93400f
Pranav Addepalli <pranavaddepalli@Pranavs-MBP.home>
2019-01-21 11:35:27 -0500

    .

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,15 @@
-import static spark.Spark.*;
+import static spark.Spark.get;
+import static spark.Spark.port;
 
+import java.io.File;
 import java.io.FileWriter;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateExceptionHandler;
 
 public class Main {
 
@@ -15,7 +24,16 @@ public class Main {
             String result = val + "";
             while (result.length() < 3)
                 result = "0" + result;
-            return result;
+
+            String newStr = "";
+            for (char c : result.toCharArray())
+                newStr += c + " ";
+            
+            newStr = newStr.substring(0, newStr.length() - 1);
+
+            System.out.println(result);
+
+            return setUpTemplate(newStr);
         });
     }
 
@@ -27,5 +45,22 @@ public class Main {
         return 4567;
     }
 
+    static String setUpTemplate(String num) throws Exception {
+        Configuration cfg = new Configuration(Configuration.VERSION_2_3_27);
+        cfg.setDirectoryForTemplateLoading(new File("src/main/res"));
+        cfg.setDefaultEncoding("UTF-8");
+        cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
+        cfg.setLogTemplateExceptions(false);
+        cfg.setWrapUncheckedExceptions(true);
+
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("num", num);
+
+        Template temp = cfg.getTemplate("index.ftlh");
+        StringWriter writer = new StringWriter();
+        temp.process(map, writer);
+       // System.out.println(writer.toString());
+        return writer.toString();
+    }
 
 }
